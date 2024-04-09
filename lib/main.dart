51b1395949a5b0e1caaac6eb67e8bb671d7428f4
icicle_4 <dwayne.han4@gmail.com>
2024-04-09 16:55:03 +0900

    feat: MediaQuery 활용 중간에 배치

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 
 void main() {
   runApp(const MyApp());
@@ -22,6 +23,23 @@ class SensorApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    final centerX = MediaQuery.of(context).size.width / 2 - 50;
+    final centerY = MediaQuery.of(context).size.height / 2 - 50;
+
+    return Scaffold(
+      body: Stack(
+        children: [
+          Positioned(
+            left: centerX,
+            top: centerY,
+            child: Container(
+              width: 100,
+              height: 100,
+              color: Colors.red,
+            ),
+          )
+        ],
+      ),
+    );
   }
 }
